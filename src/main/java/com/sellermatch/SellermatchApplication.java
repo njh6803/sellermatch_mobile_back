104bcc967819ff91b3b3a583daaf999962148a98
YeonSu <whatpull@gmail.com>
2021-05-04 13:48:16 +0900

    first commit

---
+++ src/main/java/com/sellermatch/SellermatchApplication.java
@@ -0,0 +1,13 @@
+package com.sellermatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellermatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellermatchApplication.class, args);
+    }
+
+}
