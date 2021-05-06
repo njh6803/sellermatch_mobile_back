eaf428cca781b6568e64ed4bf0ae6e80e39cd7ad
YeonSu <whatpull@gmail.com>
2021-05-06 16:44:47 +0900

    회원 컨트롤러 페이지처리 기타 통신 개발

--- src/main/java/com/sellermatch/process/member/controller/MemberController.java
+++ src/main/java/com/sellermatch/process/member/controller/MemberController.java
@@ -3,10 +3,13 @@ package com.sellermatch.process.member.controller;
 import com.sellermatch.process.member.domain.Member;
 import com.sellermatch.process.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class MemberController {
@@ -14,9 +17,30 @@ public class MemberController {
     @Autowired
     public MemberRepository memberRepository;
 
-    @GetMapping("/")
-    public List<Member> selectMember() {
-        return memberRepository.findAll();
+    @GetMapping("/member")
+    public Page<Member> selectMember() {
+        Pageable sortedByName = PageRequest.of(0, 3);
+        return (Page<Member>) memberRepository.findAll(sortedByName);
+    }
+
+    @GetMapping("/member/list")
+    public Page<Member> selectMemberList(Pageable pageable) {
+//        Pageable sortedByName = PageRequest.of(0, 3);
+        return memberRepository.findAll(pageable);
+    }
+
+    @PostMapping("/member")
+    public Member insertMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    @DeleteMapping("/member")
+    public String deleteMember(Integer memIdx) {
+        String result = "fail";
+        memberRepository.findById(memIdx).ifPresent(temp -> {
+            memberRepository.delete(temp);
+        });
+        return result;
     }
 
 }
--- src/main/java/com/sellermatch/process/member/repository/MemberRepository.java
+++ src/main/java/com/sellermatch/process/member/repository/MemberRepository.java
@@ -1,6 +1,12 @@
 package com.sellermatch.process.member.repository;
 
 import com.sellermatch.process.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> { }
+public interface MemberRepository extends PagingAndSortingRepository<Member, Integer> {
+
+    Page<Member> findAll(Pageable pageable);
+
+}
