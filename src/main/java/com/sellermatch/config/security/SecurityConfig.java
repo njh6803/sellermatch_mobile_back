61dbfa52903952b01d1518a61b0414fbd31fd12e
Quan <timid130@naver.com>
2021-05-07 17:08:57 +0900

    Spring Security 설치

---
+++ src/main/java/com/sellermatch/config/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.sellermatch.config.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+                "/swagger-resources", "/configuration/security",
+                "/swagger-ui.html", "/webjars/**","/swagger/**");
+    }
+}
--- src/main/java/com/sellermatch/process/member/controller/MemberController.java
+++ src/main/java/com/sellermatch/process/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.sellermatch.process.member.controller;
 
 import com.sellermatch.process.member.domain.Member;
 import com.sellermatch.process.member.repository.MemberRepository;
+import com.sellermatch.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,21 +12,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MemberController {
 
     @Autowired
     public MemberRepository memberRepository;
 
+    @Autowired
+    public JWTUtil jwtUtil;
+
     @GetMapping("/member")
     public Page<Member> selectMember() {
         Pageable sortedByName = PageRequest.of(0, 3);
+        List<String> roles = new ArrayList<>();
+        roles.add("ROLE_ADMIN");
+        String aaa = jwtUtil.createToken("1",roles);
+
+        String bbb = jwtUtil.getUserPk(aaa);
+
+        System.out.println(aaa);
+        System.out.println(bbb);
+
         return (Page<Member>) memberRepository.findAll(sortedByName);
     }
 
     @GetMapping("/member/list")
-    public Page<Member> selectMemberList(Pageable pageable) {
-//        Pageable sortedByName = PageRequest.of(0, 3);
+    public Page<Member> selectMemberList(Pageable pageable, String token) {
+
+        Boolean ccc = jwtUtil.validateToken(token);
+
+        System.out.println(ccc);
         return memberRepository.findAll(pageable);
     }
 
