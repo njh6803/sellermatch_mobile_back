b399cc27e1433c482e717e541565bfb7a8780651
njh4803 <njh4803@naver.com>
2021-05-14 20:10:58 +0900

    메일유틸 추가

---
+++ src/main/java/com/sellermatch/util/MailUtil.java
@@ -0,0 +1,10 @@
+package com.sellermatch.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MailUtil {
+
+}
