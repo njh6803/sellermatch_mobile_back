9e38a2c9a5c56e015854be1ebcac0913965dc322
njh4803 <njh4803@naver.com>
2021-06-08 14:03:06 +0900

    댓글,문의하기 등록

--- src/main/java/com/sellermatch/process/reply/controller/ReplyController.java
+++ src/main/java/com/sellermatch/process/reply/controller/ReplyController.java
@@ -76,6 +76,20 @@ public class ReplyController {
             return result;
         }
 
+        if (!Util.isEmpty(reply.getReplyParentMemId())) {
+            reply.setReplyParentMemId(reply.getReplyParentMemId());
+        } else {
+            reply.setReplyParentMemId(reply.getReplyWriter());
+        }
+
+        if (reply.getReplyParent() != 0) {
+            reply.setReplyParent(reply.getReplyParent());
+            reply.setReplyDepth("1");
+        } else {
+            reply.setReplyParent(replyRepository.getSeq()+1);
+            reply.setReplyDepth("0");
+        }
+
         result.setContent(replyRepository.save(reply));
         return result;
     }
--- src/main/java/com/sellermatch/process/reply/repository/ReplyRepository.java
+++ src/main/java/com/sellermatch/process/reply/repository/ReplyRepository.java
@@ -3,8 +3,12 @@ package com.sellermatch.process.reply.repository;
 import com.sellermatch.process.reply.domain.Reply;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ReplyRepository extends PagingAndSortingRepository<Reply, Integer> {
     Page<Reply> findAll(Pageable pageable);
+
+    @Query(value = "SELECT max(Reply.replyId) FROM Reply")
+    int getSeq();
 }
