7427adb0308e2a7b895b9a2eacb2c67c765a2d70
whatpull <whatpull@gmail.com>
2021-06-10 04:34:41 +0900

    스크랩 등록, 삭제 관련 수정

--- src/main/java/com/sellermatch/process/scrap/repository/ScrapRepository.java
+++ src/main/java/com/sellermatch/process/scrap/repository/ScrapRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface ScrapRepository extends PagingAndSortingRepository<Scrap, Integer> {
 
     Page<Scrap> findAll(Pageable pageable);
 
     Page<Scrap> findAllByMemIdx(Pageable pageable, int memIdx);
+
+    Optional<Scrap> findByMemIdxAndProjIdx(Integer memIdx, Integer projIdx);
 
     int countByMemIdxAndProjIdx(Integer memIdx, Integer projIdx);
 }
