0e12e25fa6254232d2cd152fd22ca2f5171404d4
YeonSu <whatpull@gmail.com>
2021-06-18 21:17:51 +0900

    운영 데이터베이스 변경

--- src/main/java/com/sellermatch/SellermatchApplication.java
+++ src/main/java/com/sellermatch/SellermatchApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableAsync
 @SpringBootApplication
 public class SellermatchApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SellermatchApplication.class, args);
