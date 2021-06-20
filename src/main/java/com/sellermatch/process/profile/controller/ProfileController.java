dad1031a37d2b56ccddd7b2a4aff6b63269303f1
whatpull <whatpull@gmail.com>
2021-06-20 18:23:30 +0900

    버그성 추가 불필요한 유효성 제거

--- src/main/java/com/sellermatch/process/profile/controller/ProfileController.java
+++ src/main/java/com/sellermatch/process/profile/controller/ProfileController.java
@@ -1,10 +1,7 @@
 package com.sellermatch.process.profile.controller;
 
-import com.sellermatch.config.constant.HashtagType;
-import com.sellermatch.config.constant.MemberType;
 import com.sellermatch.process.common.domain.CommonConstant;
 import com.sellermatch.process.common.domain.CommonDTO;
-import com.sellermatch.process.hashtag.domain.Hashtag;
 import com.sellermatch.process.profile.domain.Profile;
 import com.sellermatch.process.profile.repository.ProfileRepository;
 import com.sellermatch.process.profile.repository.ProfileRepositoryCustom;
@@ -17,8 +14,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Date;
 
 @RequiredArgsConstructor
 @RestController
@@ -63,111 +58,11 @@ public class ProfileController {
     @PutMapping("/profile")
     public CommonDTO updateProfile(Profile profile, MultipartFile profileImg) {
         CommonDTO result = new CommonDTO();
-
-        // 자기소개 : NULL 체크
-        if (Util.isEmpty(profile.getProfileIntro())) {
-            ControllerResultSet.errorCode(result, CommonConstant.ERROR_NULL_121);
-            return result;
-        }
-        // 자기소개 : 길이 체크 (50자 이상 1000자 이하)
-//        if (!Util.isLengthChk(profile.getProfileIntro(), 50 , 1000)) {
-//            ControllerResultSet.errorCode(result, CommonConstant.ERROR_LENGTH_122);
-//            return result;
-//        }
-
-        if (profile.getProfileSort().equalsIgnoreCase(MemberType.SELLER.label)) {
-            // 매출규모 : NULL 체크
-            if (Util.isEmpty(profile.getProfileVolume())){
-                ControllerResultSet.errorCode(result, CommonConstant.ERROR_NULL_123);
-                return result;
-            }
-            // 매출규모 : 숫자형식 체크
-            if (!Util.isNum(String.valueOf(profile.getProfileVolume()))){
-                ControllerResultSet.errorCode(result, CommonConstant.ERROR_FORMAT_124);
-                return result;
-            }
-            // 매출규모 : 길이 체크 ( 0원 부터 1조원 까지)
-            if (!Util.isLengthChk(String.valueOf(profile.getProfileVolume()),0,13)) {
-                ControllerResultSet.errorCode(result, CommonConstant.ERROR_LENGTH_125);
-                return result;
-            }
-        }
-
-        // 등록지역 : NULL 체크
-        if (Util.isEmpty(profile.getProfileNation())) {
-            ControllerResultSet.errorCode(result, CommonConstant.ERROR_NULL_126);
-            return result;
-        }
-        // 상품분류 : NULL 체크
-        if (Util.isEmpty(profile.getProfileIndus())){
-            ControllerResultSet.errorCode(result, CommonConstant.ERROR_NULL_127);
-            return result;
-        }
-        // 사업자번호 : NULL 체크
-        if (Util.isEmpty(profile.getProfileBizNum())){
-            ControllerResultSet.errorCode(result, CommonConstant.ERROR_NULL_128);
-            return result;
-        }
-        // 사업자번호 : 사업자번호형식 체크
-        if (!Util.isValid(profile.getProfileBizNum())){
-            ControllerResultSet.errorCode(result, CommonConstant.ERROR_FORMAT_129);
-            return result;
-        }
-        // 사업자유형 : NULL 체크
-        if (Util.isEmpty(profile.getProfileBizSort())) {
-            ControllerResultSet.errorCode(result, CommonConstant.ERROR_NULL_130);
-            return result;
-        }
-        if (profile.getProfileSort().equalsIgnoreCase(MemberType.SELLER.label)) {
-            // 해시태그 중복체크
-            if (Util.isEmpty(profile.getProfileHashtag())) {
-                String[] hashtagList = profile.getProfileHashtag().split(",");
-                if (Util.hashtagDuplicateCheck(result, hashtagList)) return result;
-            }
-        }
-
         profileRepository.findById(profile.getProfileIdx()).ifPresentOrElse(temp -> {
+            // 유효성 체크 확인
             ProjectDto projectDto = new ProjectDto();
-
-            if(!Util.isEmpty(profileImg)) projectDto.setProfileImgFile(profileImg);
-            profile.setProfileEditDate(new Date());
-            projectDto.getProfile().setProfileIntro(profile.getProfileIntro());
-            if (profile.getProfileSort().equalsIgnoreCase(MemberType.SELLER.label)) {
-                projectDto.getProfile().setProfileVolume(profile.getProfileVolume());
-                if (!Util.isEmpty(profile.getProfileHashtag())) {
-                    Hashtag tagProfile = new Hashtag();
-                    tagProfile.setFrstRegistDt(new Date());
-                    tagProfile.setFrstRegistMngr(temp.getProfileMemId());
-                    tagProfile.setHashType(HashtagType.PROFILE.label);
-                    tagProfile.setId(temp.getProfileId());
-                    projectDto.setProfileHashtag(tagProfile);
-                }
-            }
-            projectDto.getProfile().setProfileNation(profile.getProfileNation());
-            projectDto.getProfile().setProfileIndus(profile.getProfileIndus());
-            projectDto.getProfile().setProfileBizNum(profile.getProfileBizNum());
-            projectDto.getProfile().setProfileBizSort(profile.getProfileBizSort());
-            profile.setProfileState(temp.getProfileState());
-            profile.setProfileChChk(temp.getProfileChChk());
-            projectDto.getProfile().setProfileCh(profile.getProfileCh());
-            profile.setProfileRegDate(temp.getProfileRegDate());
-            profile.setProfileSaleChk(temp.getProfileSaleChk());
-            profile.setProfileBizCerti(temp.getProfileBizCerti());
-            profile.setProfileCareer(temp.getProfileCareer());
-            profile.setProfileSort(temp.getProfileSort());
-            profile.setProfileGrade(temp.getProfileGrade());
-            profile.setProfileMemId(temp.getProfileMemId());
-            profile.setProfileId(temp.getProfileId());
-            profile.setProfileRname(temp.getMemRname());
-            profile.setProfileChChkDate(temp.getProfileChChkDate());
-            profile.setProfileHit(temp.getProfileHit());
-            if (!Util.isEmpty(profile.getProfilePhoto())) {
-                projectDto.getProfile().setProfileCh(profile.getProfilePhoto());
-            } else {
-                profile.setProfileCh(temp.getProfilePhoto());
-            }
-
             projectDto.setProfile(profile);
+            if(!Util.isEmpty(profileImg)) projectDto.setProfileImgFile(profileImg);
             try {
                 result.setContent(profileService.insertAndUpdateProfile(projectDto));
             } catch (Exception e) {
@@ -177,12 +72,4 @@ public class ProfileController {
         return result;
     }
 
-/*    @DeleteMapping("/profile")
-    public CommonDTO deleteProfile(Profile profile) {
-        CommonDTO result = new CommonDTO();
-        profileRepository.findById(profile.getProfileIdx()).ifPresentOrElse(temp ->{
-            profileRepository.delete(profile);
-        }, () -> {});
-        return result;
-    }*/
 }
